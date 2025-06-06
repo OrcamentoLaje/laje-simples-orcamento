0a65eef75d4dc1c6d7820a115a1c1df60e7f6c36
GibaDevIA <sistemas.campos@gmail.com>
2025-06-06 11:47:50 -0300

    Update3 App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import TelaDeTransicao from "@/components/TelaDeTransicao";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
+// src/App.tsx ou onde define suas rotas
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -20,6 +22,7 @@ const App = () => (
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/orcamento" element={<Index />} />
+          <Route path="/transicao" element={<TelaDeTransicao />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
